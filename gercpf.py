9fd532b7b7691e11fad46e5daeb3cde4a631102f
Near Shelby <74830167+nearshelby-yt@users.noreply.github.com>
2021-02-20 16:28:43 -0300

    Update gercpf.py

--- gercpf.py
+++ gercpf.py
@@ -2,5 +2,5 @@ from requests import get
 
 def GeradorCPF(message):
 
-    cnpj = get('http://geradorapp.com/api/v1/cpf/generate?token=f01e0024a26baef3cc53a2ac208dd141').json()['data']['number_formatted']
-    return f'*CPF: *`{cpf}`'
+    number_formatted = get('http://geradorapp.com/api/v1/cpf/generate?token=f01e0024a26baef3cc53a2ac208dd141').json()['data']['number_formatted']
+    return f'*CPF: *`{number_formatted}`'
