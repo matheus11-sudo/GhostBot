7a094123631f4556d3db1fcf7f4d6dfc35772417
Luca'zz Dev <luc4zzd3v@gmail.com>
2021-02-23 14:09:19 -0300

    Tratamento de futuros problemas

--- gercpf.py
+++ gercpf.py
@@ -1,6 +1,10 @@
-from requests import get
+import requests
 
 def GeradorCPF(message):
 
-    number_formatted = get('http://geradorapp.com/api/v1/cpf/generate?token=f01e0024a26baef3cc53a2ac208dd141').json()['data']['number_formatted']
-    return f'*CPF: *`{number_formatted}`'
+    request = requests.get('http://geradorapp.com/api/v1/cpf/generate?token=f01e0024a26baef3cc53a2ac208dd141').json()
+    dados = request.get('data')
+    numero = dados.get('number_formatted')
+
+    return f'''
+CPF: {numero}'''
