b464f3f36a54fdc1c473270d5baa1c06f808efc2
Near Shelby <74830167+nearshelby-yt@users.noreply.github.com>
2021-02-26 16:31:41 -0300

    Update id_user.py

--- id_user.py
+++ id_user.py
@@ -1,4 +1,11 @@
 def UserID(message):
 
-    return f'*Nome*: `{message.from_user.first_name}`\n*Username*: `{message.from_user.username}`\n*ChatID*: `{message.from_user.id}`'
+    return f'''
+*>>> Ola {message.from_user.first_name} <<<*
+
+*Nome*: `{message.from_user.first_name}`
+*Username*: `{message.from_user.username}`
+*ChatID*: `{message.from_user.id}`
+
+*>>> Identificado {message.from_user.first_name} <<<*'''
 
